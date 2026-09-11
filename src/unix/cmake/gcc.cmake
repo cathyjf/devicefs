8488352d266f7a6893008e132e75fce1a14e71b5
Cathy J. Fitzpatrick <cathy@cathyjf.com>
2026-09-10 21:31:25 -0700

    terminal: generalize gcc toolchain

---
+++ src/unix/cmake/gcc.cmake
@@ -0,0 +1,37 @@
+find_program(GCC_C_COMPILER
+    NAMES gcc-16
+    HINTS /opt/homebrew/opt/gcc/bin
+    REQUIRED
+)
+
+get_filename_component(CMAKE_C_COMPILER "${GCC_C_COMPILER}" REALPATH)
+get_filename_component(GCC_BIN_DIRECTORY "${CMAKE_C_COMPILER}" DIRECTORY)
+get_filename_component(GCC_C_COMPILER_FILENAME "${CMAKE_C_COMPILER}" NAME)
+if(GCC_C_COMPILER_FILENAME MATCHES "^gcc(-.*)$")
+    set(GCC_VERSION_SUFFIX "${CMAKE_MATCH_1}")
+endif()
+
+set(CMAKE_CXX_COMPILER "${GCC_BIN_DIRECTORY}/g++${GCC_VERSION_SUFFIX}")
+
+set(CMAKE_AR "${GCC_BIN_DIRECTORY}/gcc-ar${GCC_VERSION_SUFFIX}")
+set(CMAKE_C_COMPILER_AR "${CMAKE_AR}")
+set(CMAKE_CXX_COMPILER_AR "${CMAKE_AR}")
+
+set(CMAKE_RANLIB "${GCC_BIN_DIRECTORY}/gcc-ranlib${GCC_VERSION_SUFFIX}")
+set(CMAKE_C_COMPILER_RANLIB "${CMAKE_RANLIB}")
+set(CMAKE_CXX_COMPILER_RANLIB "${CMAKE_RANLIB}")
+
+set(CMAKE_NM "${GCC_BIN_DIRECTORY}/gcc-nm${GCC_VERSION_SUFFIX}")
+
+find_program(GNU_OBJCOPY
+    NAMES gobjcopy objcopy
+    HINTS /opt/homebrew/opt/binutils/bin
+    OPTIONAL
+)
+if(GNU_OBJCOPY)
+    get_filename_component(CMAKE_OBJCOPY "${GNU_OBJCOPY}" REALPATH)
+    get_filename_component(GNU_BINUTILS_DIRECTORY "${CMAKE_OBJCOPY}" DIRECTORY)
+
+    set(CMAKE_OBJDUMP "${GNU_BINUTILS_DIRECTORY}/objdump")
+    set(CMAKE_STRIP "${GNU_BINUTILS_DIRECTORY}/strip")
+endif()
